1a3effebbd4699db610fcf616088e81cbfcb45f4
zwl <ideven@mail.ustc.edu.cn>
2021-01-28 12:12:02 +0800

    add lcm test case

---
+++ my-llvm-driver/tests/loopinvhoist_3.c
@@ -0,0 +1,13 @@
+int main(){
+    int num = 0 ,i,j;
+    int a ;
+    int b ;
+    for(int i=0;i<10;i++){     
+       if(b == a ){
+           b = 3;
+       }else
+       {
+           b = 9;
+       }
+    }
+} 
